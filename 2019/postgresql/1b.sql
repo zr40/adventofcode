c73d7c0ccce00f7998718771de2a1678ebd27be5
Matthijs van der Vleuten <git@zr40.nl>
2019-12-01 11:03:31 +0100

    Solve day 1

---
+++ 2019/postgresql/1b.sql
@@ -0,0 +1,14 @@
+begin;
+create schema aoc;
+set search_path to aoc;
+
+create function fuel(mass integer) returns integer immutable language sql as $$
+    select case when mass / 3 - 2 > 0 then (mass / 3 - 2) + fuel(mass / 3 - 2) else 0 end;
+$$;
+
+create table input (line text);
+\copy input from input/1
+
+select sum(fuel(line::integer)) from input;
+
+rollback;
