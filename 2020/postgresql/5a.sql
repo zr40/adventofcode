ca76e42ec9e2ab9418f99d1ca71e233797e4882b
Matthijs van der Vleuten <git@zr40.nl>
2020-12-05 10:29:51 +0100

    Simplify day 5 solution

--- 2020/postgresql/5a.sql
+++ 2020/postgresql/5a.sql
@@ -9,21 +9,21 @@ create temporary table input (line text not null);
 
 with passes as (
     select
-        case when substr(line, 1, 1) = 'F' then 0 else 64 end +
-        case when substr(line, 2, 1) = 'F' then 0 else 32 end +
-        case when substr(line, 3, 1) = 'F' then 0 else 16 end +
-        case when substr(line, 4, 1) = 'F' then 0 else 8 end +
-        case when substr(line, 5, 1) = 'F' then 0 else 4 end +
-        case when substr(line, 6, 1) = 'F' then 0 else 2 end +
-        case when substr(line, 7, 1) = 'F' then 0 else 1 end as r,
+        case when substr(line, 1, 1) = 'F' then 0 else 512 end +
+        case when substr(line, 2, 1) = 'F' then 0 else 256 end +
+        case when substr(line, 3, 1) = 'F' then 0 else 128 end +
+        case when substr(line, 4, 1) = 'F' then 0 else 64 end +
+        case when substr(line, 5, 1) = 'F' then 0 else 32 end +
+        case when substr(line, 6, 1) = 'F' then 0 else 16 end +
+        case when substr(line, 7, 1) = 'F' then 0 else 8 end +
 
         case when substr(line, 8, 1) = 'L' then 0 else 4 end +
         case when substr(line, 9, 1) = 'L' then 0 else 2 end +
-        case when substr(line, 10, 1) = 'L' then 0 else 1 end as c
+        case when substr(line, 10, 1) = 'L' then 0 else 1 end as id
 
     from input
 )
-select max(r * 8 + c)
+select max(id)
 from passes;
 
 
