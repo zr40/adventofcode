7c7bc65ab4d9f87227431b5c21f306c5e637c20c
Matthijs van der Vleuten <git@zr40.nl>
2020-12-13 10:32:19 +0100

    Remove the function

--- 2020/postgresql/13b.sql
+++ 2020/postgresql/13b.sql
@@ -7,23 +7,6 @@ create temporary table input (line text, line_number integer generated always as
 \copy input (line) from input/13
 
 
-create function combine_interval(current bigint, start bigint, other_id bigint, other_pos bigint) returns table (num bigint, min bigint, max bigint) language sql as $$
-    with recursive seq as (
-        select start::bigint as pos
-
-        union all
-
-        select seq.pos + current
-        from seq
-    ), minmax as (
-        select pos
-        from seq
-        where (pos + other_pos) % other_id = 0
-        limit 2
-    )
-    select max(pos) - min(pos), min(pos), max(pos) from minmax;
-$$;
-
 with recursive bus as (
     select id::bigint, pos-1 as pos
     from input, regexp_split_to_table(line, ',') with ordinality as r(id, pos)
@@ -36,7 +19,22 @@ with recursive bus as (
     select num, bus.pos, c.min, c.max
     from intervals
     cross join lateral (select id, pos from bus where bus.pos > intervals.pos order by bus.pos limit 1) as bus
-    cross join lateral combine_interval(i, start, bus.id, bus.pos) as c
+    cross join lateral (
+        with recursive seq as (
+            select start::bigint as pos
+
+            union all
+
+            select seq.pos + i
+            from seq
+        ), minmax as (
+            select pos
+            from seq
+            where (pos + bus.pos) % bus.id = 0
+            limit 2
+        )
+        select max(pos) - min(pos), min(pos), max(pos) from minmax
+    ) as c(num, min, max)
 )
 select max(answer) from intervals;
 
