70a9f6bcf9202283f7cf85f0b13d17ef60b51879
Matthijs van der Vleuten <git@zr40.nl>
2021-12-04 09:13:13 +0100

    Solve day 4

---
+++ 2021/postgresql/4a.sql
@@ -0,0 +1,60 @@
+begin;
+create schema aoc;
+set search_path to aoc;
+set jit to off;
+
+create temporary table input (line integer generated always as identity, value text not null);
+\copy input (value) from input/4
+
+create temporary table draw (number integer not null, line integer not null primary key);
+create temporary table board (id integer not null, x integer not null, y integer not null, number integer not null);
+create index on board(id, x);
+create index on board(id, y);
+
+insert into draw (number, line)
+select number::integer, ord
+from input,
+     regexp_split_to_table(value, ',') with ordinality as r(number, ord)
+where line = 1;
+
+insert into board (id, x, y, number) select (line + 4) / 6, ord, (line - 2) % 6, number::integer
+from input,
+     regexp_split_to_table(trim(leading ' ' from value), ' +') with ordinality as r(number, ord)
+where line > 1 and number <> '';
+
+create function iterate() returns integer language plpgsql as $$
+declare
+    draws integer := 0;
+    result integer;
+    sum integer;
+begin
+    loop
+        draws = draws + 1;
+
+        select id into result
+        from board
+        inner join draw on board.number = draw.number
+        where draw.line <= draws
+        group by grouping sets ((id, x), (id, y))
+        having count(*) = 5;
+
+        if result is not null then
+            sum = sum(number)
+                from board
+                where id = result
+                and not exists (
+                    select
+                    from draw
+                    where draw.line <= draws
+                    and draw.number = board.number
+                );
+
+            return sum * (select number from draw where line = draws);
+        end if;
+    end loop;
+end
+$$;
+
+select iterate();
+
+rollback;
