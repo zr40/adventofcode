0fed6741763c4fc5cc90309dd1e4bd2f5c72f370
Matthijs van der Vleuten <git@zr40.nl>
2021-12-12 09:09:52 +0100

    Solve day 7

---
+++ 2021/postgresql/7b.sql
@@ -0,0 +1,21 @@
+begin;
+create schema aoc;
+set search_path to aoc;
+set jit to off;
+
+create temporary table input (value text not null);
+\copy input (value) from input/7
+
+with positions as (
+    select pos::integer from input, string_to_table(value, ',') as p(pos)
+)
+select sum((abs(pos-target) * (abs(pos-target) + 1)) / 2), target
+from positions
+cross join generate_series((select min(pos) from positions), (select max(pos) from positions)) as gs(target)
+group by target
+order by sum
+limit 1
+;
+
+
+rollback;
