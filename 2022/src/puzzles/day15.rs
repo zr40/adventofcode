061dc93805765fbff4b8f612773eeef65933ed59
Matthijs van der Vleuten <git@zr40.nl>
2022-12-15 21:31:25 +0100

    Improve run time by avoiding allocation

--- 2022/src/puzzles/day15.rs
+++ 2022/src/puzzles/day15.rs
@@ -40,14 +40,13 @@ struct Sensor {
     sensor_y: i64,
     beacon_x: i64,
     beacon_y: i64,
-    possible_locations: Vec<(i64, i64)>,
+    possible_distance: i64,
 }
 
 fn solve_b_for(input: &str, range: i64) -> i64 {
     let mut sensors = vec![];
 
     for line in input.lines() {
-        let mut possible_locations = vec![];
         let (_, line) = line.split_once("Sensor at x=").unwrap();
         let (sensor_x, line) = line.split_once(", y=").unwrap();
         let (sensor_y, line) = line.split_once(": closest beacon is at x=").unwrap();
@@ -60,51 +59,66 @@ fn solve_b_for(input: &str, range: i64) -> i64 {
 
         let distance = (sensor_x - beacon_x).abs() + (sensor_y - beacon_y).abs();
         let possible_distance = distance + 1;
-        for i in 0..possible_distance {
-            possible_locations.push((sensor_x + possible_distance - i, sensor_y + i));
-            possible_locations.push((sensor_x - i, sensor_y + possible_distance - i));
-            possible_locations.push((sensor_x - possible_distance + i, sensor_y - i));
-            possible_locations.push((sensor_x + i, sensor_y - possible_distance + i));
-        }
 
         sensors.push(Sensor {
             sensor_x,
             sensor_y,
             beacon_x,
             beacon_y,
-            possible_locations,
+            possible_distance,
         });
     }
 
     for sensor in &sensors {
-        for (x, y) in &sensor.possible_locations {
-            if *x < 0 || *y < 0 || *x > range || *y > range {
-                continue;
-            }
+        for i in 0..sensor.possible_distance {
+            let possible_locations = [
+                (
+                    sensor.sensor_x + sensor.possible_distance - i,
+                    sensor.sensor_y + i,
+                ),
+                (
+                    sensor.sensor_x - i,
+                    sensor.sensor_y + sensor.possible_distance - i,
+                ),
+                (
+                    sensor.sensor_x - sensor.possible_distance + i,
+                    sensor.sensor_y - i,
+                ),
+                (
+                    sensor.sensor_x + i,
+                    sensor.sensor_y - sensor.possible_distance + i,
+                ),
+            ];
 
-            let mut seen = false;
-
-            for other_sensor in &sensors {
-                if sensor.sensor_x == other_sensor.sensor_x
-                    && sensor.sensor_y == other_sensor.sensor_y
-                {
+            for (x, y) in possible_locations {
+                if x < 0 || y < 0 || x > range || y > range {
                     continue;
                 }
 
-                let other_sensor_beacon_range = (other_sensor.sensor_x - other_sensor.beacon_x)
-                    .abs()
-                    + (other_sensor.sensor_y - other_sensor.beacon_y).abs();
-                let distance_to_other_sensor =
-                    (other_sensor.sensor_x - x).abs() + (other_sensor.sensor_y - y).abs();
+                let mut seen = false;
 
-                if distance_to_other_sensor <= other_sensor_beacon_range {
-                    seen = true;
-                    break;
+                for other_sensor in &sensors {
+                    if sensor.sensor_x == other_sensor.sensor_x
+                        && sensor.sensor_y == other_sensor.sensor_y
+                    {
+                        continue;
+                    }
+
+                    let other_sensor_beacon_range = (other_sensor.sensor_x - other_sensor.beacon_x)
+                        .abs()
+                        + (other_sensor.sensor_y - other_sensor.beacon_y).abs();
+                    let distance_to_other_sensor =
+                        (other_sensor.sensor_x - x).abs() + (other_sensor.sensor_y - y).abs();
+
+                    if distance_to_other_sensor <= other_sensor_beacon_range {
+                        seen = true;
+                        break;
+                    }
                 }
-            }
 
-            if !seen {
-                return x * 4000000 + y;
+                if !seen {
+                    return x * 4000000 + y;
+                }
             }
         }
     }
