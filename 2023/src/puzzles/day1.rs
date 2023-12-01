0133b39092f89a58b0d59c3cd07d0f050798b919
Matthijs van der Vleuten <git@zr40.nl>
2023-12-01 10:56:07 +0100

    Solve day 1

---
+++ 2023/src/puzzles/day1.rs
@@ -0,0 +1,81 @@
+#[allow(dead_code)]
+const EXAMPLE_A: &str = include_str!("../input/1a_example");
+#[allow(dead_code)]
+const EXAMPLE_B: &str = include_str!("../input/1b_example");
+const INPUT: &str = include_str!("../input/1");
+
+fn solve_a_for(input: &str) -> u32 {
+    input
+        .lines()
+        .map(|line| {
+            let mut digits = line.chars().filter_map(|digit| digit.to_digit(10));
+            let first = digits.next().unwrap();
+            let last = digits.last().unwrap_or(first);
+            first * 10 + last
+        })
+        .sum()
+}
+
+fn solve_b_for(input: &str) -> u32 {
+    input
+        .lines()
+        .map(|line| {
+            let mut digits = (0..line.len()).filter_map(|index| {
+                let substr = &line[index..];
+                if substr.starts_with("one") {
+                    Some(1)
+                } else if substr.starts_with("two") {
+                    Some(2)
+                } else if substr.starts_with("three") {
+                    Some(3)
+                } else if substr.starts_with("four") {
+                    Some(4)
+                } else if substr.starts_with("five") {
+                    Some(5)
+                } else if substr.starts_with("six") {
+                    Some(6)
+                } else if substr.starts_with("seven") {
+                    Some(7)
+                } else if substr.starts_with("eight") {
+                    Some(8)
+                } else if substr.starts_with("nine") {
+                    Some(9)
+                } else {
+                    substr.chars().next().unwrap().to_digit(10)
+                }
+            });
+
+            let first = digits.next().unwrap();
+            let last = digits.last().unwrap_or(first);
+            first * 10 + last
+        })
+        .sum()
+}
+
+#[test]
+fn a_example() {
+    assert_eq!(solve_a_for(EXAMPLE_A), 142);
+}
+
+#[test]
+fn a_puzzle() {
+    assert_eq!(solve_a_for(INPUT), 53974);
+}
+
+#[test]
+fn b_example() {
+    assert_eq!(solve_b_for(EXAMPLE_B), 281);
+}
+
+#[test]
+fn b_puzzle() {
+    assert_eq!(solve_b_for(INPUT), 52840);
+}
+
+pub fn solve_a() {
+    println!("{}", solve_a_for(INPUT));
+}
+
+pub fn solve_b() {
+    println!("{}", solve_b_for(INPUT));
+}
---
+++ 2023/src/puzzles/mod.rs
@@ -0,0 +1,8 @@
+use phf::{phf_ordered_map, OrderedMap};
+
+mod day1;
+
+pub static PUZZLES: OrderedMap<&'static str, fn() -> ()> = phf_ordered_map! {
+    "1a" => day1::solve_a,
+    "1b" => day1::solve_b,
+};
