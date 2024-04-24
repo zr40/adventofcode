8e353d19c3ff8abff79dd08955d2738cca85a12f
Matthijs van der Vleuten <git@zr40.nl>
2024-04-24 11:12:00 +0200

    2015: solve day 17

---
+++ src/aoc2015/day17.rs
@@ -0,0 +1,61 @@
+use itertools::Itertools;
+
+use crate::PuzzleResult;
+
+#[cfg(test)]
+const EXAMPLE: &str = include_str!("input/17_example");
+const INPUT: &str = include_str!("input/17");
+
+fn solve_a_for(input: &str, eggnog: u16) -> usize {
+    let containers: Vec<u16> = input.lines().map(|l| l.parse().unwrap()).collect();
+
+    containers
+        .into_iter()
+        .powerset()
+        .filter(|c| c.iter().sum::<u16>() == eggnog)
+        .count()
+}
+fn solve_b_for(input: &str, eggnog: u16) -> usize {
+    let containers: Vec<u16> = input.lines().map(|l| l.parse().unwrap()).collect();
+
+    containers
+        .into_iter()
+        .powerset()
+        .filter_map(|c| {
+            if c.iter().sum::<u16>() == eggnog {
+                Some(c.len())
+            } else {
+                None
+            }
+        })
+        .min_set()
+        .len()
+}
+
+#[test]
+fn a_example() {
+    assert_eq!(solve_a_for(EXAMPLE, 25), 4);
+}
+
+#[test]
+fn a_puzzle() {
+    assert_eq!(solve_a_for(INPUT, 150), 1304);
+}
+
+#[test]
+fn b_example() {
+    assert_eq!(solve_b_for(EXAMPLE, 25), 3);
+}
+
+#[test]
+fn b_puzzle() {
+    assert_eq!(solve_b_for(INPUT, 150), 18);
+}
+
+pub fn solve_a() -> PuzzleResult {
+    solve_a_for(INPUT, 150).into()
+}
+
+pub fn solve_b() -> PuzzleResult {
+    solve_b_for(INPUT, 150).into()
+}
