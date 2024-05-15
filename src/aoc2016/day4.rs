a5f758db61349b1982cb8e2aebc50b32912966c5
Matthijs van der Vleuten <git@zr40.nl>
2024-05-15 10:49:48 +0200

    2016: solve day 4

---
+++ src/aoc2016/day4.rs
@@ -0,0 +1,130 @@
+use std::collections::BTreeMap;
+
+use itertools::Itertools;
+
+use crate::day::Day;
+use crate::puzzle_result::PuzzleResult;
+
+#[cfg(test)]
+const EXAMPLE: &str = include_str!("input/4_example");
+const INPUT: &str = include_str!("input/4");
+
+struct Room {
+    name: String,
+    sector_id: u32,
+    checksum: String,
+}
+
+impl Room {
+    fn from(line: &str) -> Room {
+        let (rest, _) = line.rsplit_once(']').unwrap();
+        let (rest, checksum) = rest.rsplit_once('[').unwrap();
+        let (name, sector_id) = rest.rsplit_once('-').unwrap();
+        let sector_id = sector_id.parse().unwrap();
+
+        Room {
+            name: name.to_owned(),
+            sector_id,
+            checksum: checksum.to_owned(),
+        }
+    }
+
+    fn checksum_valid(&self) -> bool {
+        let mut chars = BTreeMap::new();
+
+        for ch in self.name.chars() {
+            if ch == '-' {
+                continue;
+            }
+
+            chars.entry(ch).and_modify(|e| *e += 1).or_insert(1);
+        }
+
+        let checksum: String = chars
+            .iter()
+            .sorted_by_key(|(_, count)| -*count)
+            .take(5)
+            .map(|(ch, _)| ch)
+            .collect();
+
+        checksum == self.checksum
+    }
+
+    fn decrypted_name(&self) -> String {
+        self.name
+            .chars()
+            .map(|ch| match ch {
+                '-' => ' ',
+                ch => {
+                    char::from_u32((ch as u32 + self.sector_id - ('a' as u32)) % 26 + ('a' as u32))
+                        .unwrap()
+                }
+            })
+            .collect()
+    }
+}
+
+fn solve_a_for(input: &str) -> u32 {
+    input
+        .lines()
+        .filter_map(|line| {
+            let room = Room::from(line);
+            if room.checksum_valid() {
+                Some(room.sector_id)
+            } else {
+                None
+            }
+        })
+        .sum()
+}
+
+fn solve_b_for(input: &str) -> u32 {
+    input
+        .lines()
+        .find_map(|line| {
+            let room = Room::from(line);
+            if room.checksum_valid() && room.decrypted_name() == "northpole object storage" {
+                Some(room)
+            } else {
+                None
+            }
+        })
+        .unwrap()
+        .sector_id
+}
+
+#[test]
+fn a_example() {
+    assert_eq!(solve_a_for(EXAMPLE), 1514);
+}
+
+#[test]
+fn a_puzzle() {
+    assert_eq!(solve_a_for(INPUT), 409147);
+}
+
+#[test]
+fn b_example() {
+    assert_eq!(
+        Room::from("qzmt-zixmtkozy-ivhz-343[]").decrypted_name(),
+        "very encrypted name"
+    );
+}
+
+#[test]
+fn b_puzzle() {
+    assert_eq!(solve_b_for(INPUT), 991);
+}
+
+fn solve_a() -> PuzzleResult {
+    solve_a_for(INPUT).into()
+}
+
+fn solve_b() -> PuzzleResult {
+    solve_b_for(INPUT).into()
+}
+
+pub(super) static DAY: Day = Day::Separate {
+    a: solve_a,
+    b: solve_b,
+};
